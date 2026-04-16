3df6d71db1f18fb134aa30582a9c6f403c2c99cc
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-04-16 17:21:13 +0700

    Validasi supaya input tidak lebih dari 10

--- BINARYSEARCH.cpp
+++ BINARYSEARCH.cpp
@@ -5,5 +5,13 @@ int element[10];
 int nPanjang;
 int x;
 
-void input() 
-   
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya element pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
