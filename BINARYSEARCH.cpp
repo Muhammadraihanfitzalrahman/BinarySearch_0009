a1d485c9a43fdc3c215e057c18480f7404edd566
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-04-16 17:23:07 +0700

    Loop untuk mengisi array

--- BINARYSEARCH.cpp
+++ BINARYSEARCH.cpp
@@ -5,13 +5,25 @@ int element[10];
 int nPanjang;
 int x;
 
-void input()
-{
-    while (true)
-    {
+void input() {
+    while (true) {
         cout << "Masukkan banyaknya element pada array (maksimal 10): ";
         cin >> nPanjang;
 
-        if (nPanjang <= 10)
-        {
+        if (nPanjang <= 10) {
             break;
+        } else {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10, Silahkan coba lagi.\n";
+        }
+    }
+
+    cout << "\n=============================\n";
+    cout << "      Masukkan element Array\n";
+    cout << "=============================\n";
+
+
+     for (int i = 0; i < nPanjang; i++) {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
+}
